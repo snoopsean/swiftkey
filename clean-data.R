90ad86c8436d8dd3ee3a832b1f3f3cb24d6df50b
Johnny Dang <snoopsean@zoho.com>
2015-08-17 17:17:00 -0700

    still cleaning data

--- clean-data.R
+++ clean-data.R
@@ -2,5 +2,22 @@
 
 load("sample_data.RData")
 
-#to change the \" to ", just run it through cat()
+#make it all lowercase
+snews<-tolower(snews)
+
+#remove numbers and punctuation
+noNumbersOrPunctionation <-function (x) gsub("[^a-zA-Z\n\']", " ", x)
+
+#to change the \" to ", just run it through cat(), may not need this if last line takes care of it
 snews<-cat(sample_news)
+
+#collapse multiple white spaces into one space
+onespace<-function (x) gsub( "\\s+", " ", x, perl = TRUE )
+snews <- onespace(snews)
+
+#get rid of leading and trailing whitespace
+trim <- function (x) gsub("^\\s+|\\s+$", "", x, perl=TRUE)
+snews<-trim(snews)
+
+#remove numbers and punctuation
+noNumbersOrPunctionation <-function (x) gsub("[^a-zA-Z\n\']", " ", x)
