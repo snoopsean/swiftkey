50fbce78562e08118fbdaf1a72984fcaf6b4e56f
Johnny Dang <snoopsean@zoho.com>
2015-08-22 16:29:37 -0700

    close to having a model where i can get a basic prediction

--- clean-data.R
+++ clean-data.R
@@ -18,6 +18,9 @@ trim <- function (x) gsub("^\\s+|\\s+$", "", x, perl=TRUE)
 onespace<-function (x) gsub( "\\s+", " ", x, perl = TRUE )
 
 #run all three cleaning functions, and make it lowercase
+#if i do it this way don't becomes don t, do i want that
+# or do i want dont to be one gram? if so, ill change the noNumPunc
+# to replace numbers and punctuation with a "" instead of " "
 sample_clean<-tolower(trim(onespace(noNumPunc(sample_joined))))
 
 rm(sample_joined)
@@ -27,11 +30,13 @@ library(stylo)
 sample_clean<-txt.to.words(sample_clean)
 
 #get our ngrams
+#could clean this up to do it through a loop, and remove things from ram during each loop
 gram1<-make.ngrams(sample_clean,1)
 gram2<-make.ngrams(sample_clean,2)
 gram3<-make.ngrams(sample_clean,3)
 gram4<-make.ngrams(sample_clean,4)
 gram5<-make.ngrams(sample_clean,5)
+gram6<-make.ngrams(sample_clean,6)
 
 rm(sample_clean)
 
@@ -41,11 +46,21 @@ gram2table<-sort(table(gram2),decreasing=TRUE)
 gram3table<-sort(table(gram3),decreasing=TRUE)
 gram4table<-sort(table(gram4),decreasing=TRUE)
 gram5table<-sort(table(gram5),decreasing=TRUE)
+gram6table<-sort(table(gram6),decreasing=TRUE)
 
-save(gram1,gram2,gram3,gram4,gram5,gram1table,gram2table,gram3table,gram4table,
-     gram5table,file = "ngrams.Rdata")
+save(gram1,file="ngram1.Rdata") ; rm(gram1)
+save(gram2,file="ngram2.Rdata") ; rm(gram2)
+save(gram3,file="ngram3.Rdata") ; rm(gram3)
+save(gram4,file="ngram4.Rdata") ; rm(gram4)
+save(gram5,file="ngram5.Rdata") ; rm(gram5)
+save(gram6,file="ngram6.Rdata") ; rm(gram6)
 
-rm(gram1,gram2,gram3,gram4,gram5,gram1table,gram2table,gram3table,gram4table,
-   gram5table)
+#save(gram1,gram2,gram3,gram4,gram5,gram6,file = "ngrams.Rdata")
+#rm(gram1,gram2,gram3,gram4,gram5,gram6)
 
-load("ngrams.Rdata")
+save(gram1table,file="ngram1table.Rdata") ; rm(gram1table)
+save(gram2table,file="ngram2table.Rdata") ; rm(gram2table)
+save(gram3table,file="ngram3table.Rdata") ; rm(gram3table)
+save(gram4table,file="ngram4table.Rdata") ; rm(gram4table)
+save(gram5table,file="ngram5table.Rdata") ; rm(gram5table)
+save(gram6table,file="ngram6table.Rdata") ; rm(gram6table)
---
+++ train.R
@@ -0,0 +1,53 @@
+#train.R
+
+load("ngram1table.Rdata")
+load("ngram2table.Rdata")
+load("ngram3table.Rdata")
+load("ngram4table.Rdata")
+
+# at this point, should I remove anything with a frequency of 1?
+# that would shorten my model, but make it less predictable
+# since the RAM limit for shiny apps is 1 gig, Ill need to do that
+
+#even just using 2,3,4 tables, I still have about 1.42 gigs of ram used
+
+# here is the code to remove anything with a low probability (for instance, one occurence)
+
+# so I want to take the input, and search for the first occurance in the gram4table
+# that is the most likely match. if nothng found, go to 3table, then 2table
+
+#its already sorted, so i need the extract the actual text strings
+gram1tableOpt<-rownames(gram1table) #; rm(gram1table)
+gram2tableOpt<-rownames(gram2table) #; rm(gram2table)
+gram3tableOpt<-rownames(gram3table) #; rm(gram3table)
+
+# we would get inputbigram, inputunigram. if its smaller, then we set to null
+# next step is to take the last 2 words input by user, and search my trigrams for a match
+# I will find the first match (since its sorted), and return the last word of that trigram
+# if no match is found, take the last word input by user, and search my bigrams
+# return the last word of the bigram with a match
+# if no match is found, return the most common word (in this case "the")
+# i will later change it to look at other parts of the sentence, but for now Ill keep it at that
+
+#input from user, sample for now
+tinput2<-"i dasdasuaksnd"
+tinput1<-"like"
+# create a regular expression to pass into grep
+t2<-paste("^",tinput2," ",sep="")
+t1<-paste("^",tinput1," ",sep="")
+
+#return the first match
+fm<-head(grep(t2,gram3tableOpt,value=TRUE),1)
+
+#if no match
+if(length(fm)==0)
+{
+  fm<-head(grep(t1,gram2tableOpt,value=TRUE),1)
+}
+
+# if still no match
+#return "the"
+fm<-"the"
+
+# input words is: words
+if(sum(gram1tableOpt==words)>=1) { r<- 6}
